4c8e1c84bb7d18133091b65a0a0fbe079163f916
Carl Helmertz <helmertz@gmail.com>
2012-11-10 14:30:28 +0100

    setup.py, make install, readme
    Signed-off-by: Carl Helmertz <helmertz@gmail.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 test: clean
 	@python test/test_subtitler.py
 
+install: clean
+	@python setup.py install
+
+package: clean
+	@python setup.py sdist
+
 clean:
 	@rm -f *.pyc test/*.pyc
 
-.PHONY: test clean
+.PHONY: test clean package install
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='subtitler',
+    version='0.1',
+    description='Rename subtitle files to match their movie counterpart',
+    author='Carl Helmertz',
+    author_email='helmertz@gmail.com',
+    url='https://github.com/chelmertz/subtitler',
+    scripts=['subtitler.py']
+)
